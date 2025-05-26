ebce7834b23f5406ec9dab0dd0837e2e88d252dd
abhi-323 <ak00873@gmail.com>
2025-05-26 13:59:13 +0530

    updated home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,37 +4,10 @@ import HeroSection from "../components/HeroSection";
 import List from "../components/List";
 
 const Home = () => {
-  const [tableItems, setTableItems] = useState([]);
-
-  useEffect(() => {
-    const fetchAccountRequests = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:8080/api/accountRequest/getAll"
-        );
-        const formattedData = response.data.map((item) => ({
-          avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg", // default avatar
-          name: item.user.name,
-          email: item.user.email,
-          accountType: item.requestedType,
-          branch: item.branch,
-          ifscCode: item.ifscCode,
-          status: item.status,
-          pan: item.pan,
-        }));
-        setTableItems(formattedData);
-      } catch (error) {
-        console.error("Failed to fetch account requests:", error);
-      }
-    };
-
-    fetchAccountRequests();
-  }, []);
 
   return (
     <>
       <HeroSection />
-      <List tableItems={tableItems} />
     </>
   );
 };
